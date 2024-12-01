b273888a8ec404c8336678ccb72e66c20af125fa
9claws <9claws.azat@gmail.com>
2024-12-01 07:49:25 +0500

    first commit

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import requests
 
 response = requests.post('http://127.0.0.1:5000/advertisement',
-                         json={'title': 'Test', 'description': 'Yekaterinburg', 'owner_id': 'Andrey'})
+                         json={'title': 'Test', 'description': 'Surgut', 'owner_id': 'Azat'})
 
 print(response.status_code)
 print(response.json())
